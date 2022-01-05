60a8fdb9393a66f62acb9ca2efa1bebbc5e3d6d6
Bryan Santos <brymsantos@gmail.com>
2022-01-04 21:07:54 -0800

    solved server exercises2 p1

---
+++ J5/ServerExercises2/Exercise1.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+const setColor = (browser = "UNKNOWN") => {
+  let color = "#000000";
+  if (browser === "chrome") color = "#0000FF";
+  else if (browser === "firefox") color = "#FF0000";
+
+  return `
+    <style>
+    h1 {
+      color: ${color};
+      }
+    </style>
+      <h1>Welcome ${browser} user</h1>
+  
+  `;
+};
+
+app.get("/browser", (req, res) => {
+  const browserString = req.get("User-Agent").toUpperCase();
+  if (browserString.includes("CHROME")) {
+    res.send(setColor("chrome"));
+  } else if (browserString.includes("FIREFOX")) {
+    res.send(setColor("firefox"));
+  }
+  res.send(setColor());
+});
+
+app.listen(3000);
